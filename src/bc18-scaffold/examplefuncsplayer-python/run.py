c2215a3922e948812402d4fed45e18fe9136e271
Aditya Addepalli <30334566+Dyex719@users.noreply.github.com>
2018-01-11 16:11:33 +0530

    Uploaded files

---
+++ src/bc18-scaffold/examplefuncsplayer-python/run.py
@@ -0,0 +1,52 @@
+import battlecode as bc
+import random
+import sys
+import traceback
+
+print("pystarting")
+
+# A GameController is the main type that you talk to the game with.
+# Its constructor will connect to a running game.
+gc = bc.GameController()
+directions = list(bc.Direction)
+
+print("pystarted")
+
+# It's a good idea to try to keep your bots deterministic, to make debugging easier.
+# determinism isn't required, but it means that the same things will happen in every thing you run,
+# aside from turns taking slightly different amounts of time due to noise.
+random.seed(6137)
+
+# let's start off with some research!
+# we can queue as much as we want.
+gc.queue_research(bc.UnitType.Rocket)
+gc.queue_research(bc.UnitType.Worker)
+gc.queue_research(bc.UnitType.Knight)
+
+my_team = gc.team()
+
+while True:
+    # We only support Python 3, which means brackets around print()
+    print('pyround:', gc.round())
+
+    # frequent try/catches are a good idea
+    try:
+        # walk through our units:
+        for unit in gc.my_units():
+            # if unit.unit_type == bc.UnitType.Worker:
+                d = random.choice(directions)
+                gc.move_robot(unit.id, d)
+
+    except Exception as e:
+        print('Error:', e)
+        # use this to show where the error was
+        traceback.print_exc()
+
+    # send the actions we've performed, and wait for our next turn.
+    # this is the final yield function.
+    gc.next_turn()
+
+    # these lines are not strictly necessary, but it helps make the logs make more sense.
+    # it forces everything we've written this turn to be written to the manager.
+    sys.stdout.flush()
+    sys.stderr.flush()
